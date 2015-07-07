7ec8d7f7a32d609f13de5449ee2775434dacbb22
aeremin <aeremin@maxus.ru>
2015-07-07 12:43:12 +0300

    done

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,17 @@
 class FakeArray
-
+  include Enumerable
   # you'll need a splat in this class somewhere
-
-
-
+  
+  def initialize(*args)
+    @f = []
+	args.each {|a| @f << a}
+  end
+  
+  def [](index)
+    @f[index]
+  end  
+  
+  def each
+    @f.each {|f| yield f}
+  end
 end

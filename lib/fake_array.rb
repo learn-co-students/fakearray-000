01247089d27a83b9cbadeef7b8de94ff642c8581
Rigz777 <rigzgoodmusic@gmail.com>
2015-12-01 00:22:12 -0500

    long way

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,19 @@
-class FakeArray < Array
+class FakeArray 
 
   def initialize(*things)
-    super(things)
+    @array = []
+    things.each {|thing| @array << thing}
   end
 
+  def [](index)
+    @array[index]
+  end
+
+  def each
+    @array.each {|item| yield item}
+  end
+    
+  def first
+    @array[0]    
+  end  
 end

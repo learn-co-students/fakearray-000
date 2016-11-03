0b9cb29a78adc93f4a38fb9e47f165621bba19aa
Lucas Fields <lqf2b8@gmail.com>
2016-11-03 15:47:16 +0000

    finished

--- lib/fake_array.rb
+++ lib/fake_array.rb
@@ -1,7 +1,22 @@
 class FakeArray
 
   # you'll need a splat in this class somewhere
+  def initialize(*items)
+    @array = *items
+  end
 
+  def [](num)
+    @array[num]
+  end
 
+  def each
+    @array.each do |item |
+      yield item
+    end
+  end
 
-end
+  def first
+    @array[0]
+  end
+
+end 
